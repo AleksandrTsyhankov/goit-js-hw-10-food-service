d98307ff495d84deb7a6d6dbdac5602afa0c000f
AleksandrTsyhankov <74210910+AleksandrTsyhankov@users.noreply.github.com>
2021-06-27 22:02:50 +0300

    -hw done v1

--- src/index.js
+++ src/index.js
@@ -1,1 +1,58 @@
 import './sass/main.scss';
+import menu from '../menu.json'
+import menuTemplate from './templates/menu.hbs'
+
+const menuList = document.querySelector('.menu')
+const checkBox = document.querySelector('#theme-switch-toggle')
+const body = document.querySelector('body')
+const Theme = {
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme',
+};
+let currentTheme = localStorage.getItem('theme')
+
+checkBox.addEventListener('change', onCheckBoxChange)
+
+if (!currentTheme) {
+    checkBox.checked = false
+    localStorage.setItem('theme', Theme.LIGHT)
+} else
+    if (currentTheme === Theme.DARK) {
+        checkBox.checked = true
+        body.classList.add(Theme.DARK)
+
+    }
+
+
+function onCheckBoxChange(e) {
+    if (checkBox.checked) {
+        body.classList.remove(Theme.LIGHT)
+        body.classList.add(Theme.DARK)
+        localStorage.setItem('theme', Theme.DARK)
+    }
+    else if (!checkBox.checked) {
+        body.classList.remove(Theme.DARK)
+        body.classList.add(Theme.LIGHT)
+        localStorage.setItem('theme', Theme.LIGHT)
+    }
+}
+
+const menuToInsert = menuTemplate(menu)
+menuList.insertAdjacentHTML('beforeend', menuToInsert)
+
+
+console.log(menuList)
+console.log(menuTemplate(menu))
+
+
+
+
+
+
+
+
+
+
+
+
+
